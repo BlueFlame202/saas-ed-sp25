dc12cd59a46964aaa6eedcbf88328d6bc55dd10e
Aathreya Kadambi <aathreyakadambi@gmail.com>
2025-02-09 03:36:15 -0800

    first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://aathreyakadambi.vercel.app/',
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  })],
+  output: "server"
+});
